2fbcc2d90ea21fb6f9d4e8dc6e6d011ea7df3784
Mong Ramos Jr <mongramosjr@gmail.com>
2023-11-19 01:28:01 +0800

    refactor device and myself fragments

--- app/src/main/java/com/squidsentry/mobile/ui/devices/DevicesFragment.kt
+++ app/src/main/java/com/squidsentry/mobile/ui/devices/DevicesFragment.kt
@@ -1,4 +1,4 @@
-package com.squidsentry.mobile.ui.myself
+package com.squidsentry.mobile.ui.devices
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.squidsentry.mobile.databinding.FragmentMyselfBinding
+import com.squidsentry.mobile.databinding.FragmentDevicesBinding
 
-class NotificationsFragment : Fragment() {
+class DevicesFragment : Fragment() {
 
-    private var _binding: FragmentMyselfBinding? = null
+    private var _binding: FragmentDevicesBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class NotificationsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+        val dashboardViewModel =
+            ViewModelProvider(this).get(DevicesViewModel::class.java)
 
-        _binding = FragmentMyselfBinding.inflate(inflater, container, false)
+        _binding = FragmentDevicesBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textMyself
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.textDevices
+        dashboardViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
--- app/src/main/java/com/squidsentry/mobile/ui/myself/MyselfFragment.kt
+++ app/src/main/java/com/squidsentry/mobile/ui/myself/MyselfFragment.kt
@@ -1,4 +1,4 @@
-package com.squidsentry.mobile.ui.devices
+package com.squidsentry.mobile.ui.myself
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.squidsentry.mobile.databinding.FragmentDevicesBinding
+import com.squidsentry.mobile.databinding.FragmentMyselfBinding
 
-class DashboardFragment : Fragment() {
+class MyselfFragment : Fragment() {
 
-    private var _binding: FragmentDevicesBinding? = null
+    private var _binding: FragmentMyselfBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class DashboardFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+        val notificationsViewModel =
+            ViewModelProvider(this).get(MyselfViewModel::class.java)
 
-        _binding = FragmentDevicesBinding.inflate(inflater, container, false)
+        _binding = FragmentMyselfBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.textMyself
+        notificationsViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
